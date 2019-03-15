5d8ecef07d3c59ba373f8ff31ba1fe653a7d7294
SuNNjek <sunnerlp@gmail.com>
2019-03-15 16:40:08 +0100

    Added crash code

---
+++ Crash/Crash/ViewModel/MainVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Essentials;
+
+namespace Crash.ViewModel
+{
+	public class MainVM : ViewModelBase
+	{
+		private RelayCommand _sendMailCommand;
+		public RelayCommand SendMailCommand => _sendMailCommand ?? (_sendMailCommand = new RelayCommand(SendMailAsync));
+
+		private async void SendMailAsync()
+		{
+			EmailMessage message = new EmailMessage
+			{
+				Subject = "Crash",
+				Body = "Test",
+				BodyFormat = EmailBodyFormat.PlainText,
+				To = new List<string> { "test@example.com" },
+				Cc = new List<string> { String.Empty },
+				Bcc = new List<string> { String.Empty },
+			};
+
+			await Email.ComposeAsync(message);
+		}
+	}
+}
